2c8295d99df544738d8ae7a3c8d25936916a1376
MatiasCra <cravchikm@gmail.com>
2022-04-27 20:02:38 -0300

    clase 18 cambios

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# render es otra forma de cargar templates que veremos en próximas clases
+# from django.shortcuts import render
 
-# Create your views here.
+from AppCoder.models import Estudiante
+from django.http import HttpResponse
+from django.template import loader
+
+
+def crear_estudiante(request):
+    estudiante = Estudiante(
+        nombre="Juancito", apellido="Perez", email="juan.perez@gmail.com"
+    )
+    # Con .save() guardamos la instancia en la bd.
+    estudiante.save()
+    return HttpResponse(f"Se creó a {estudiante.nombre} {estudiante.apellido}")
