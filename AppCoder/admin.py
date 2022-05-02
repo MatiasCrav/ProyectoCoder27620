e061a0640dae1bf85c4fc91a3c81c4b39e2b3cad
MatiasCra <cravchikm@gmail.com>
2022-05-02 20:12:27 -0300

    admin

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Curso, Entregable, Estudiante, Profesor
 
-# Register your models here.
+admin.site.register(Curso)
+admin.site.register(Entregable)
+admin.site.register(Estudiante)
+admin.site.register(Profesor)
