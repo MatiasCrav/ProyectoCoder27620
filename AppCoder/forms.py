50c14e9b599a893ac415626622562c67da3e6d44
MatiasCra <cravchikm@gmail.com>
2022-05-11 20:02:03 -0300

    typo

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -10,7 +10,7 @@ class FormCurso(forms.Form):
 
 class FormRegistrarse(UserCreationForm):
     email = forms.EmailField(
-        # Se puede especificat como tiene que ser el widet y los atributos del tag en el 
+        # Se puede especificar como tiene que ser el widet y los atributos del tag en el 
         # html
         widget=forms.EmailInput(
             attrs={"placeholder": "email...", "style": "background-color: antiquewhite"}
